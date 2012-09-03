297b0455c1c992db1ce56c83062f92a35b67df22
Vincent B <vinceguit@gmail.com>
2012-09-03 10:14:12 +0200

    Added basic functions and a server proxy.

---
+++ vinceguit/roleplay/src/ClientProxy.java
@@ -0,0 +1,14 @@
+package vinceguit.roleplay.src;
+
+import net.minecraftforge.client.MinecraftForgeClient;
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import cpw.mods.fml.common.Mod.Instance;
+
+public class ClientProxy extends CommonProxy
+{
+
+	public void registerRenderInformation() 
+	{  
+	}
+
+}
